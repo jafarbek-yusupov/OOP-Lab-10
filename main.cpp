f561a91d5cdb7b6a89bf9be71492e30dda14372b
j.yusupov <j.yusupov@newu.uz>
2025-03-20 12:38:22 +0500

    bptzd

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <list>
 #include <numeric>
-#include <algorithm>
+// #include <algorithm>
 #define str string
 using namespace std;
 
@@ -11,7 +11,6 @@ void task1() {
     int n; cin >> n; vector<int> v(n); for(int& i : v){ cin >> i;} int vv; cin >> vv;
     for(int& i : v){ if(i!=vv){ cout << i << " "; i=-1;}} cout << '\n';
 }
-
 bool dgts(const str& w){ for(char i : w){ if(i >= '0' && i <= '9'){ return true;}} return false;}
 void task2() {
     int n; cin >> n;
@@ -20,7 +19,6 @@ void task2() {
     v.erase(it, v.end());
     for(const str& i : v){ cout << i << " ";} cout << '\n';
 }
-
 void task3() {
     list<double> lst;
     int n; cin >> n;
@@ -38,10 +36,24 @@ void task3() {
     for(const auto& i : lst){ cout << i << " ";} cout << "\n";
 }
 
+void task4(){
+    int n; cin >> n; vector<double> v(n); for(auto& i : v){ cin >> i;}
+    cout << "List: "; for(const auto& i : v){ cout << i << " ";}
+    for_each(v.begin(), v.end(), [](double& x) { x *= 1.10;});
+    cout << "\nIncreased: "; for(const auto& i : v){ cout << i << " ";} cout << "\n";
+    auto mn = *min_element(v.begin(), v.end(), [](double i, double z) { return i<z; });
+    auto mx = *max_element(v.begin(), v.end(), [](double i, double z) { return i<z; });
+    cout << "min = " << mn << "\nmax = " << mx << "\n";
+
+    double t; cin >> t;
+    int c = count_if(v.begin(), v.end(), [t](double i) { return i >= t; }); cout  << ">=threshold: "<< c << "\n";
+}
+
 int main(){
     // task1();
     // task2();
-    task3();
+    // task3();
+    task4();
     return 0;
 }
 
